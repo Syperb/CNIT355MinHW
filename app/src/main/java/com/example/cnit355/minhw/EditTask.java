3de8a9185cc06fbb3f7a49e9aaf12d08428607a3
Bradley Persson <e-brad95@hotmail.com>
2016-11-30 02:43:52 -0500

    Decided to work on saving files instead, partially complete for edit task. I'll work more on it in the morning, I need sleep now.

--- app/src/main/java/com/example/cnit355/minhw/EditTask.java
+++ app/src/main/java/com/example/cnit355/minhw/EditTask.java
@@ -1,20 +1,32 @@
 package com.example.cnit355.minhw;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class EditTask extends Fragment {
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_edit_task, container,
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_edit_task, container,
                 false);
 
         ImageView btnBack = (ImageView) rootView.findViewById(R.id.btnBack);
@@ -30,6 +42,31 @@ public class EditTask extends Fragment {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                EditText editTaskComplete = (EditText) rootView.findViewById(R.id.editCompletion);
+                TextView txtTaskClass = (TextView) rootView.findViewById(R.id.txtClass);
+                TextView txtTaskDesc = (TextView) rootView.findViewById(R.id.txtDescription);
+                TextView txtTaskName = (TextView) rootView.findViewById(R.id.txtName);
+                TimePicker timePicker = (TimePicker) rootView.findViewById(R.id.editDueTime);
+                SeekBar seekBar = (SeekBar) rootView.findViewById(R.id.progressBar);
+                File edittedTask = new File(rootView.getContext().getFilesDir().getAbsolutePath() + "/Tasks/" + txtTaskName);
+
+
+
+
+                if (edittedTask.exists()){
+                    //create dialog to ask user if they want to overwrite
+                }
+                else{
+                    try {
+                        OutputStreamWriter outFs = new OutputStreamWriter(getActivity().openFileOutput(txtTaskName.getText().toString(), MODE_PRIVATE));
+                        //outFs.write();
+                        outFs.close();
+
+                    } catch (IOException e) {e.printStackTrace();}
+                }
+
+
 
 
                 //Functionality of Save Button
--- app/src/main/java/com/example/cnit355/minhw/MainActivity.java
+++ app/src/main/java/com/example/cnit355/minhw/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.cnit355.minhw;
 
+import android.content.ContextWrapper;
+import android.os.Environment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     mainTaskView fragmentA;
     EditTask fragmentB;
     activity_settings fragmentC;
+    File taskDir = new File("/Tasks");
+    TextView taskName = (TextView) findViewById(R.id.txtName);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,20 @@ public class MainActivity extends AppCompatActivity {
         fragmentA = (mainTaskView) getSupportFragmentManager().findFragmentById(R.id.MainFragment);
         fragmentB = new EditTask();
         fragmentC = new activity_settings();
+
+        //tries to make a directory to store files in
+        taskDir = new File(this.getApplicationContext().getFilesDir().getAbsolutePath() + taskDir);
+
+
+        try {
+            taskDir.mkdir();
+        } catch (Exception e){e.printStackTrace();}
+
+        //Toast.makeText(this, this.getApplicationContext().getFilesDir().getAbsolutePath(), Toast.LENGTH_SHORT).show();
+
+
+
+
     }
 
     public void onFragmentChanged(int index) {
